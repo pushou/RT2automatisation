1b9dddcfac9961fca1ca86c7e487937bb0b226b4
pushou <jeanmarc.pouchoulon@gmail.com>
2015-09-19 14:36:05 +0200

    typo

--- create_user_fg.sh
+++ create_user_fg.sh
@@ -6,6 +6,5 @@ adduser user1 # creation de l'utilisateur user1
 usermod -g git user1 # rattachemement de user1 au groupe git
 adduser user2 # creation de l'utilisateur user2
 usermod -g git user2 # rattachemement de user2 au groupe git
-adduser user2
 su - user1 -c "git config --global user.email 'user1' ; git config --global user.name  'user1'"
 su - user2 -c "git config --global user.email 'user2' ; git config --global user.name  'user2'"
